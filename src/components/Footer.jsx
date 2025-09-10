fb1f015c7b3d154cdb0f980bdb2312544d067e10
Shiva <67689627+Harshsinghal2415@users.noreply.github.com>
2025-09-10 10:28:16 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 export default function Footer() {
   return (
     <div className='footer'>
-      <p>© 2022 Mansi Singhal</p>
-      <LinkedInIcon onClick={() => window.open('https://www.linkedin.com/in/mansi-singhal-2001/')} />
-      <GitHubIcon onClick={() => window.open('https://github.com/mansisinghal11')} />
+      <p>© 2022 Shiva Singhal</p>
+      <LinkedInIcon onClick={() => window.open('www.linkedin.com/in/shiva-singhal-b89202227')} />
+      <GitHubIcon onClick={() => window.open('https://github.com/Harshsinghal2415')} />
     </div>
   );
 }
